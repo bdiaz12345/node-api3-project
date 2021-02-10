5a13a4db860e06d60b8e353fbb7c1eeb04eb15be
bdiaz12345 <bdiazmusic5@gmail.com>
2021-02-10 15:14:08 -0500

    mvp

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,50 @@
+const Posts = require('../posts/posts-model')
+const Users = require('../users/users-model')
+
 function logger(req, res, next) {
   // do your magic!
+  console.log(req)
+  next()
 }
 
-function validateUserId(req, res, next) {
+const validateUserId = async (req, res, next) => {
   // do your magic!
+  const {id} = req.params
+  try {
+    const user = await Users.getById(id)
+    if (!user) {
+      res.status(400).json({message: 'No user with that id'})
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (e) {
+    res.status(500).json({message: `Server error: ${e}`})
+  }
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  if (!req.body.name) {
+    res.status(400).json({message: 'name required'})
+  } else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  if (!req.body || !req.params.id) {
+    res.status(400).json({message: 'body and id need to be correct'})
+  } else {
+    next()
+  }
 }
 
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
 // do not forget to expose these functions to other modules
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,41 +1,90 @@
 const express = require('express');
+const mw = require('../middleware/middleware')
+const Users = require('./users-model')
+const Posts = require('../posts/posts-model')
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  Users.get(req.query)
+    .then(users => {
+      res.status(200).json(users)
+    })
+    .catch(error => {
+      res.status(500).json({message: 'error retrieving users'})
+    })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', mw.validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  res.status(200).json(req.user)
 });
 
-router.post('/', (req, res) => {
+router.post('/', mw.validateUser, (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  Users.insert(req.body)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(error => {
+      res.status(500).json({message: 'error adding user'})
+    })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', mw.validateUser, mw.validateUserId, (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  Users.update(req.params.id, req.body)
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(error => {
+      res.status(500).json({message: 'error updating user'})
+    })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', mw.validateUserId, (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  Users.remove(req.params.id)
+    .then(count => {
+      res.status(200).json({message: 'user deleted'})
+    })
+    .catch(error => {
+      res.status(500).json({message: 'error'})
+    })
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', mw.validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  Users.getUserPosts(req.params.id)
+    .then(posts => {
+      res.status(200).json(posts)
+    })
+    .catch(error => {
+      res.status(500).json({message: 'error retrieving user posts'})
+    })
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', mw.validateUserId, mw.validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  const postInfo = { ...req.body, user_id: req.params.id }
+  Posts.insert(postInfo)
+    .then(post => {
+      res.status(201).json(post)
+    })
+    .catch(error => {
+      res.status(500).json({message: 'error posting post'})
+    })
 });
+
+module.exports = router
 
 // do not forget to export the router
